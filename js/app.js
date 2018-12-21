8c407123fd33a2baf0deda0f0dbdb1d2d611e946
tokoko1234 <t_japaridze2@cu.edu.ge>
2018-12-20 16:12:09 -0800

    Added grid class

--- js/app.js
+++ js/app.js
@@ -44,3 +44,64 @@ class Element{
 
 
 }
+
+class Grid{
+    //matricis sheqmna
+    static createArrayMatrix(){
+        let matrix = [];
+        for(let i = 0; i<20; i++){
+            matrix[i] = [];
+            for(let j = 0; j<20; j++){
+                matrix[i][j] = null;
+            }
+        }
+        return matrix;
+    }
+
+
+    constructor(){
+
+        this.grid = Grid.createArrayMatrix();
+        this.container = document.querySelector('#container');
+    }
+    //axali elementis chagdeba
+    addNewElement (Element){
+        this.grid[Element.X][Element.Y] = Element;
+        this.container.appendChild(Element.newElement);
+    }
+
+    //gvelis wertilis shecvla
+    pointReplace(element,x,y){
+        element.X = x;
+        element.Y = y;
+        this.grid[element.X][element.Y] = null;
+        this.grid[x][y] = element;
+
+    }
+
+    //Shevamowmot matrica
+    checkTheGrid(xCoord, yCoord){
+        if(xCoord == 20 || yCoord === 20){
+            return "hit";
+        }
+        //console.log(xCoord + " " + yCoord);
+        //console.log(this.grid[xCoord][yCoord] + " " + xCoord + " " + yCoord);
+        if(typeof this.grid[xCoord][yCoord] !== 'undefined') {
+
+            if (this.grid[xCoord][yCoord] == null) {
+                return "null";
+            } else if (this.grid[xCoord][yCoord].id == -5) {
+                return "apple";
+            } else {
+                return "snake";
+            }
+        }
+        else {
+            return "hit";
+        }
+    }
+
+
+
+
+}
